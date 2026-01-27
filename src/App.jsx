87943a0cf7c91cbdf50829ecf2ecbd88a15a1167
alex Kochens <ofwonoalex256@gmail.com>
2026-01-27 23:09:54 +0300

    refactor: add confirm task deletion

--- src/App.jsx
+++ src/App.jsx
@@ -5,7 +5,13 @@ import TaskList from './components/TaskList';
 const App = () => {
   // Function to delete the task
   const deleteTasks = (id) => {
-    setTasks(tasks.filter((task) => task.id !== id));
+    const confirmDelete = window.confirm(
+      'Are you sure yo want to delete this task?',
+    );
+
+    if (confirmDelete) {
+      setTasks(tasks.filter((task) => task.id !== id));
+    }
   };
   const [tasks, setTasks] = useState([]);
   return (
